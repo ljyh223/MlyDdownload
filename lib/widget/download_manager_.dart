1bccf049b9825984805704f40211ee5879865c6f
ljyh <3439426154@qq.com>
2023-08-29 16:58:49 +0800

    init

--- lib/widget/download_manager_.dart
+++ lib/widget/download_manager_.dart
@@ -1,17 +1,3 @@
-import 'dart:async';
-
-// StreamController<List<String>> progressStreamController = StreamController<List<String>>();
-// Stream<List<String>> get progressStream => progressStreamController.stream;
-
-
-// 创建多个流，每个流用于一个文件的下载进度
-StreamController<Map<String, FileDownloadInfo>> fileStreamController = StreamController<Map<String, FileDownloadInfo>>();
-Stream<Map<String, FileDownloadInfo>> get fileProgressStream => fileStreamController.stream;
-void disposeProgressController() {
-  fileStreamController.close();
-}
-
-
 class FileDownloadInfo {
   final String id;
   final String fileName;
